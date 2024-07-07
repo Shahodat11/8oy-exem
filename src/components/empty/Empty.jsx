1fe1ec79be2eec1e6f7592d1b10daa4a4c34db7e
unknown <shahodatusmonjonova@gmail.com>
2024-07-07 10:36:41 +0500

    kamchilikk

--- src/components/empty/Empty.jsx
+++ src/components/empty/Empty.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 const Empty = ({ text }) => {
   return (
     <div className="empty">
-      <img src={wishlist} alt="" />
+      <img className="empty" src={wishlist} alt="" />
       <h2 className="empty-h2">We didn't find what you were looking for :(</h2>
       <NavLink to={`/`}>
         <button className="button12">На главную</button>
